b2e4e7240d267e9420c0fbabce2c625e1b03dafa
Aundra Miller <miller.aundra@gmail.com>
2019-04-11 11:34:51 -0700

    Use Redis pub/sub pattern for scalability

--- RealTimeCharts_Server/Controllers/ChartController.cs
+++ RealTimeCharts_Server/Controllers/ChartController.cs
@@ -8,6 +8,9 @@ using Microsoft.AspNetCore.SignalR;
 using RealTimeCharts_Server.DataStorage;
 using RealTimeCharts_Server.HubConfig;
 using RealTimeCharts_Server.TimerFeatures;
+using StackExchange.Redis;
+using Newtonsoft.Json;
+using RealTimeCharts_Server.Models;
 
 namespace RealTimeCharts_Server.Controllers
 {
@@ -17,17 +20,25 @@ namespace RealTimeCharts_Server.Controllers
     public class ChartController : ControllerBase
     {
         private IHubContext<ChartHub> _hub;
+        private IConnectionMultiplexer _redis;
 
-        public ChartController(IHubContext<ChartHub> hub)
+        public ChartController(IHubContext<ChartHub> hub, IConnectionMultiplexer redis)
         {
             _hub = hub;
+            _redis = redis;
         }
 
         public IActionResult Get()
         {
-            var timerManager = new TimerManager(() => _hub.Clients.All.SendAsync("transferchartdata", DataManager.GetData()));
+            ISubscriber sub = _redis.GetSubscriber();
+            var timerManager = new TimerManager(() => sub.Publish("chartDataUpdate", JsonConvert.SerializeObject(DataManager.GetData())));
+
+            sub.Subscribe("chartDataUpdate", (channel, message) => {
+                _hub.Clients.All.SendAsync("transferchartdata", JsonConvert.DeserializeObject<List<ChartModel>>(message));
+            });
 
             return Ok(new { Message = "Request Completed" });
+
         }
     }
 }
--- RealTimeCharts_Server/Startup.cs
+++ RealTimeCharts_Server/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using RealTimeCharts_Server.HubConfig;
+using StackExchange.Redis;
 
 namespace RealTimeCharts_Server
 {
@@ -36,6 +37,7 @@ namespace RealTimeCharts_Server
             });
             services.AddSignalR();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddSingleton<IConnectionMultiplexer>(x => ConnectionMultiplexer.Connect("localhost"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
